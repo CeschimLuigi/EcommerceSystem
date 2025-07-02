92589a47da51ae3a18c2bf53704e9f2afc498e5e
CeschimLuigi <luigifaggeceschim@gmail.com>
2025-07-02 20:21:27 -0300

    homologando BACKEND para deploy V1.1

--- BACKEND/catalogo-service/src/config/db.js
+++ BACKEND/catalogo-service/src/config/db.js
@@ -7,6 +7,7 @@ async function connectMongo() {
     try {
         await mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost:27018/catalogo");
         console.log("✅ MongoDB conectado com sucesso");
+        console.log(mongoose.connection.name);
     } catch (err) {
         console.error("Erro ao conectar no MongoDB:", err);
     }
